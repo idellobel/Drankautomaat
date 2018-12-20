2a5e5584a7772ca331b82c7298e827de037e10f0
idellobel <ivan.dellobel@gmail.com>
2018-12-20 15:40:51 +0100

    Initial commit

---
+++ DrankAutomaatLib/Klassen.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrankAutomaatLib
+{
+    public enum Status { Kies, Betaal, Koop, Neem }
+
+    public class Drankje
+    {
+        private decimal drankprijs;
+        public decimal Drankprijs { get; set; }
+
+        private string dranknaam;
+        public string Dranknaam { get; set; }
+
+        public Drankje() { }
+
+        public Drankje(string dranknaam, decimal drankprijs)
+        {
+            Dranknaam = dranknaam;
+            Drankprijs = drankprijs;
+        }
+
+        public override string ToString()
+        {
+            return String.Format(" {0} "
+               , Dranknaam);
+        }
+
+    }
+
+    public class Wisselgeld
+    {
+        private decimal geldwaarde;
+        public decimal Geldwaarde { get; set; }
+
+        int[] aantalstukken = new int[6];
+        public int[] Aantalstukken
+        {
+            get { return aantalstukken; }
+            set { aantalstukken = value; }
+        }
+
+        public Wisselgeld() { }
+
+        public Wisselgeld(decimal geldwaarde)
+        {
+            Geldwaarde = geldwaarde;
+        }
+
+        public decimal OmzettingCent(Wisselgeld geld)
+        {
+            Geldwaarde = Math.Round(Geldwaarde * 100);
+            return Geldwaarde;
+        }
+
+        public override string ToString()
+        {
+            return String.Format(" {0} "
+               , Geldwaarde);
+        }
+
+    }
+
+    public class Drankenautomaat
+    {
+        public static Status Status { get; set; }
+        public Wisselgeld geld;
+        public Drankje keuze;
+
+        public decimal automaatInworp;
+        public decimal totaleInworp;
+
+        public string automaatKeuze;
+        public decimal automaatDrankprijs;
+        public string automaatDranknaam;
+        public List<Drankje> dranken;
+
+        public List<Wisselgeld> euro;
+        public List<Wisselgeld> cent;
+
+        public string berekenWisselgeld;
+        public decimal terug;
+        //private decimal resterendBedrag;
+
+        public Drankenautomaat()
+        {
+            keuze = new Drankje();
+
+            dranken = new List<Drankje>();
+            dranken.Add(new Drankje("Cola", 1.50M));
+            dranken.Add(new Drankje("Water", 1.20M));
+            dranken.Add(new Drankje("Koffie", 2.00M));
+            dranken.Add(new Drankje("Soep", 1.75M));
+
+            geld = new Wisselgeld();
+
+            euro = new List<Wisselgeld>();
+            euro.Add(new Wisselgeld(2M));
+            euro.Add(new Wisselgeld(1M));
+
+            cent = new List<Wisselgeld>();
+            cent.Add(new Wisselgeld(50M));
+            cent.Add(new Wisselgeld(20M));
+            cent.Add(new Wisselgeld(10M));
+            cent.Add(new Wisselgeld(5M));
+
+        }
+
+        public decimal TotaleInworp(Wisselgeld geld)
+        {
+            automaatInworp = geld.Geldwaarde * 1.00M;
+            if (automaatInworp > 2.00M)
+            {
+                automaatInworp = automaatInworp / 100;
+            }
+            totaleInworp += automaatInworp;
+            return totaleInworp;
+
+        }
+        public decimal AnnulerenInworp()
+        {
+            totaleInworp = 0;
+            return totaleInworp;
+        }
+
+        public string HuidigeKeuze(Drankje keuze)
+        {
+            automaatKeuze = keuze.Dranknaam + "( €" + keuze.Drankprijs + ")";
+            automaatDranknaam = keuze.Dranknaam;
+            return automaatKeuze;
+        }
+        /// <summary>
+        /// Melding: berekenen wisselgeld
+        /// </summary>
+        /// <returns>terug.ToString()</returns>
+        public string BerekenTerug()
+        {
+            terug = totaleInworp - automaatDrankprijs;
+            return terug.ToString();
+        }
+
+        /// <summary>
+        /// Melding: berekenen van de munten van het wisselgeld
+        /// </summary>
+        /// <param name="terug"></param>
+        /// <returns>berekenWisselgeld </returns>
+        public string BerekenWisselGeld(decimal terug)
+        {
+            decimal resterendBedrag = terug;
+            var muntstukken = new[]
+            {
+                new { geldstuk = " x € 2,00 ", waarde   = 2.00M },
+                new { geldstuk = " x € 1,00 ", waarde   = 1.00M },
+                new { geldstuk = " x € 0,50 ", waarde   = 0.50M},
+                new { geldstuk = " x € 0,20 ", waarde   = 0.20M},
+                new { geldstuk = " x € 0,10 ", waarde   = 0.10M},
+                new { geldstuk = " x € 0,05 " , waarde   = 0.05M }
+             };
+            /*
+            //Voorlopig niet geslaagde uitvoering
+            int[] aantalstukken = new int[6];
+           
+            for (int i = 0; i < muntstukken.Length; i++)
+             {
+               
+                decimal hulp = resterendBedrag;
+                     aantalstukken[i] = (int)(resterendBedrag / muntstukken[i].waarde);
+                //if (aantalstukken[i] > 0)
+                berekenWisselgeld=  "\n" + aantalstukken[i].ToString() + muntstukken[i].geldstuk;
+                     hulp = resterendBedrag - (aantalstukken[i] * muntstukken[i].waarde);
+             }
+
+            return berekenWisselgeld;
+         }*/
+
+
+            //1 - dient compacter !!
+            resterendBedrag = terug;
+            int aantalStukken = (int)(resterendBedrag / muntstukken[0].waarde);
+            if (aantalStukken > 0)
+                berekenWisselgeld = aantalStukken.ToString() + muntstukken[0].geldstuk;
+
+            //2
+            resterendBedrag -= (aantalStukken * muntstukken[0].waarde);
+            aantalStukken = (int)(resterendBedrag / muntstukken[1].waarde);
+            if (aantalStukken > 0)
+                berekenWisselgeld += "\n" + aantalStukken.ToString() + muntstukken[1].geldstuk;
+
+            //3
+            resterendBedrag -= (aantalStukken * muntstukken[1].waarde);
+            aantalStukken = (int)(resterendBedrag / muntstukken[2].waarde);
+            if (aantalStukken > 0)
+                berekenWisselgeld += "\n" + aantalStukken.ToString() + muntstukken[2].geldstuk;
+
+            //4
+            resterendBedrag -= (aantalStukken * muntstukken[2].waarde);
+            aantalStukken = (int)(resterendBedrag / muntstukken[3].waarde);
+            if (aantalStukken > 0)
+                berekenWisselgeld += "\n" + aantalStukken.ToString() + muntstukken[3].geldstuk;
+
+            //5
+            resterendBedrag -= (aantalStukken * muntstukken[3].waarde);
+            aantalStukken = (int)(resterendBedrag / muntstukken[4].waarde);
+            if (aantalStukken > 0)
+                berekenWisselgeld += "\n" + aantalStukken.ToString() + muntstukken[4].geldstuk;
+
+            //6
+            resterendBedrag -= (aantalStukken * muntstukken[4].waarde);
+            aantalStukken = (int)(resterendBedrag / muntstukken[5].waarde);
+            if (aantalStukken > 0)
+                berekenWisselgeld += "\n" + aantalStukken.ToString() + muntstukken[5].geldstuk;
+            return
+            berekenWisselgeld;
+        }
+    }
+
+ }
+
+    
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ WpfDrankAutomaat/MainWindow.xaml.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DrankAutomaatLib;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.Media;
+
+namespace WpfDrankAutomaat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Drankenautomaat automaat = new Drankenautomaat();
+        Button eurocentbutton;
+        Button eurobutton;
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        public void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Kies();
+            Inworp();
+        }
+      
+       private void Kies()
+        {
+            foreach (Drankje keuze in automaat.dranken)
+            {
+               Button drankbutton = new Button();
+                
+                spMaakEenKeuze.IsEnabled = true;
+                drankbutton.Content = keuze;
+               
+                drankbutton.Click += drankbutton_Click;
+                spMaakEenKeuze.Children.Add(drankbutton);
+                drankbutton.Width = 100;
+                drankbutton.Height = 20;
+                drankbutton.Margin = new Thickness(5, 0, 10, 55);
+            }
+            lbStatus.Content = Status.Kies;
+       }
+
+        public void Inworp()
+        {
+            foreach (Wisselgeld geld in automaat.euro)
+            {
+                eurobutton = new Button();
+                eurobutton.Content = geld;
+                eurobutton.Click += eurobutton_Click;
+                spInworpEuro.Children.Add(eurobutton);
+                spInworpEuro.IsEnabled = false;
+                eurobutton.Width = 215;
+                eurobutton.Height = 20;
+                eurobutton.Margin = new Thickness(5, 0, 10, 10);
+            }
+
+            foreach (Wisselgeld geld in automaat.cent)
+            {
+                eurocentbutton = new Button();
+                eurocentbutton.Content = geld;
+                eurocentbutton.Click += eurocentbutton_Click;
+                spInworpEuroCent.Children.Add(eurocentbutton);
+                spInworpEuroCent.IsEnabled = false;
+                eurocentbutton.Width = 100;
+                eurocentbutton.Height = 20;
+                eurocentbutton.Margin = new Thickness(5, 0, 10, 65);
+            }
+        }
+        public void drankbutton_Click(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer Drankbutton = new SoundPlayer(Resource2.Drank);
+            Drankbutton.Play();
+            Drankje c = (sender as Button).Content as Drankje;
+            automaat.automaatDrankprijs = c.Drankprijs;
+            lbDrankkeuze.Content = automaat.HuidigeKeuze(c);
+            lbStatus.Content = Status.Betaal;
+            spInworpEuro.IsEnabled = true;
+            spInworpEuroCent.IsEnabled = true;
+            spMaakEenKeuze.IsEnabled = false;
+        }
+
+        public void eurobutton_Click(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer Eurobutton = new SoundPlayer(Resource2.Munt);
+            Eurobutton.Play();
+            Wisselgeld a = (sender as Button).Content as Wisselgeld;
+            automaat.automaatInworp = a.Geldwaarde;
+            lbIngeworpenBedrag.Content = "€ " + automaat.TotaleInworp(a);
+            Kopen();
+        }
+
+        private void eurocentbutton_Click(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer Eurocentbutton = new SoundPlayer(Resource2.Munt);
+            Eurocentbutton.Play();
+
+            Wisselgeld a = (sender as Button).Content as Wisselgeld;
+            automaat.automaatInworp = a.Geldwaarde;
+            lbIngeworpenBedrag.Content = "€ " + automaat.TotaleInworp(a);
+            Kopen();
+        }
+
+        private void btKopen__Click(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer Kopenbutton = new SoundPlayer(Resource2.Jackpot);
+            Kopenbutton.Play();
+            lbStatus.Content = Status.Neem;
+            btAnnuleren.IsEnabled = false;
+            btNeemDrankje.Visibility = Visibility;
+            btNeemDrankje.IsEnabled = true;
+            lbMdedelingInhoud.Content = ("Neem uw " + automaat.automaatDranknaam
+                   + "\nterug € " + automaat.BerekenTerug()
+                   + "\n" + automaat.BerekenWisselGeld(automaat.terug)
+                   + "\n");
+          
+            img01.Source = new BitmapImage(new Uri("/images/"+ automaat.automaatDranknaam +".png", UriKind.Relative));
+            img01.Visibility = Visibility.Visible;
+            btnKopen.IsEnabled = false;
+        }
+        
+        private void Kopen()
+        {
+            if (automaat.totaleInworp >= automaat.automaatDrankprijs)
+            {
+                btnKopen.IsEnabled = true;
+                lbStatus.Content = Status.Koop;
+                spInworpEuro.IsEnabled = false;
+                spInworpEuroCent.IsEnabled = false;
+            }
+        }
+        private void btAnnuleren_Click(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer Annulerenbutton = new SoundPlayer(Resource2.Annuleren);
+            Annulerenbutton.Play();
+            Annuleren();
+        }
+        private void Annuleren()
+        {
+            spMaakEenKeuze.Children.Clear();
+            lbDrankkeuze.Content = "";
+            lbIngeworpenBedrag.Content = "€ 0.00";
+            automaat.AnnulerenInworp();
+            automaat.berekenWisselgeld = "";
+            btnKopen.IsEnabled = false;
+            btAnnuleren.IsEnabled = true;
+            lbMdedelingInhoud.Content = "";
+            btNeemDrankje.Visibility = Visibility.Hidden;
+            Kies();
+        }
+
+        private void btNeemDrankje_Click(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer Neembutton = new SoundPlayer(Resource2.Neem);
+            Neembutton.Play();
+            img01.Visibility = Visibility.Hidden;
+            Annuleren();
+        }
+       
+    }
+
+}
